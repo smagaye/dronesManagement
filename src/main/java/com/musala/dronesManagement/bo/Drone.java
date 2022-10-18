fdb7abe3421769e7694e066a24f4289c6d3750c2
Ada Gaye <sma.gaye94@gmail.com>
2022-10-18 10:05:41 +0000

    Business object added

---
+++ src/main/java/com/musala/dronesManagement/bo/Drone.java
@@ -0,0 +1,88 @@
+package com.musala.dronesManagement.bo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.musala.dronesManagement.enums.Model;
+import com.musala.dronesManagement.enums.State;
+
+@Entity
+public class Drone {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(unique = true, length = 100)
+	private String serialNumber;
+
+	@Enumerated(EnumType.STRING)
+	private Model model;
+	
+	
+	@Column
+	private double weight;
+	
+	@Column
+	private int battery;
+	
+	@Column
+	@Enumerated(EnumType.STRING)
+	private State state;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+	public Model getModel() {
+		return model;
+	}
+
+	public void setModel(Model model) {
+		this.model = model;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public int getBattery() {
+		return battery;
+	}
+
+	public void setBattery(int battery) {
+		this.battery = battery;
+	}
+
+	public State getState() {
+		return state;
+	}
+
+	public void setState(State state) {
+		this.state = state;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/musala/dronesManagement/enums/State.java
@@ -0,0 +1,5 @@
+package com.musala.dronesManagement.enums;
+
+public enum State {
+	IDLE, LOADING, LOADED, DELIVERING, DELIVERED, RETURNING
+}
