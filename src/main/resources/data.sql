f2820e46146fb962f531073f76d8f78254eaf582
Ada Gaye <sma.gaye94@gmail.com>
2022-10-19 23:42:20 +0000

    Seeders Medication table added

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,3 +8,13 @@ INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) value
 INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39007', 'Lightweight', 487, 24, 'DELIVERING');
 INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39008', 'Middleweight', 400, 100, 'LOADING');
 INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39009', 'Lightweight', 240, 100, 'IDLE');
+INSERT INTO MEDICATION (`code`, `image`, `weight`, `name`) values ('MED_53785k39100', 'https://api.v2/images/MED53785k39100.png', 101, 'Paracetamol');
+INSERT INTO MEDICATION (`code`, `image`, `weight`, `name`) values ('MED_53785k39101', 'https://api.v2/images/MED53785k39101.png', 240, 'Litacoldggg');
+INSERT INTO MEDICATION (`code`, `image`, `weight`, `name`) values ('MED_53785k39102', 'https://api.v2/images/MED53785k39102.png', 200, 'Mebendazole');
+INSERT INTO MEDICATION (`code`, `image`, `weight`, `name`) values ('MED_53785k39103', 'https://api.v2/images/MED53785k39103.png', 200, 'Doliparane');
+INSERT INTO MEDICATION (`code`, `image`, `weight`, `name`) values ('MED_53785k39104', 'https://api.v2/images/MED53785k39104.png', 275, 'CLOMID');
+INSERT INTO MEDICATION (`code`, `image`, `weight`, `name`) values ('MED_53785k39105', 'https://api.v2/images/MED53785k39105.png', 190, 'Vogalene');
+INSERT INTO MEDICATION (`code`, `image`, `weight`, `name`) values ('MED_53785k39106', 'https://api.v2/images/MED53785k39106.png', 289, 'Zeben 400');
+INSERT INTO MEDICATION (`code`, `image`, `weight`, `name`) values ('MED_53785k39107', 'https://api.v2/images/MED53785k39107.png', 287, 'Amodiaquine');
+INSERT INTO MEDICATION (`code`, `image`, `weight`, `name`) values ('MED_53785k39108', 'https://api.v2/images/MED53785k39108.png', 100, 'Cetamil');
+INSERT INTO MEDICATION (`code`, `image`, `weight`, `name`) values ('MED_53785k39109', 'https://api.v2/images/MED53785k39109.png', 250, 'Voltarene');
