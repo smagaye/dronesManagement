72e8f9c731f49fa3e4ac2a532a92edd312493b06
Ada Gaye <sma.gaye94@gmail.com>
2022-10-19 21:10:04 +0000

    Seeders for Drone table

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39000', 'Lightweight', 500, 100, 'LOADING');
+INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39001', 'Lightweight', 450, 67, 'LOADING');
+INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39002', 'Middleweight', 300, 88, 'RETURNING');
+INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39003', 'Heavyweight', 400, 73, 'LOADED');
+INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39004', 'Lightweight', 375, 23, 'LOADING');
+INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39005', 'Lightweight', 490, 95, 'DELIVERED');
+INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39006', 'Cruiserweight', 389, 100, 'LOADED');
+INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39007', 'Lightweight', 487, 24, 'DELIVERING');
+INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39008', 'Middleweight', 400, 100, 'LOADING');
+INSERT INTO DRONE (`serial_number`, `model`, `weight`, `battery`, `state`) values ('GR89285i39009', 'Lightweight', 240, 100, 'IDLE');
